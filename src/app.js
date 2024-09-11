719adada4b6cea18fdaa433a59e893f00f0bcf45
maxHarsh <harshdogney@gmail.com>
2024-09-12 00:40:20 +0530

    utiles code

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express  from "express";
+import cors from  "cors"
+import  CookieParse  from "cookie-parser"
+
+const app = express()
+
+app.use(cors(
+    {origin:process.env.CORS_ORIGIN,
+        credentials:true
+    }
+))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({limit:"16kb",extended:true}))
+app.use(express.static("Public"))
+app.use(CookieParse())
+
+export {app}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './env' });
+import connectDB from "./db/index.js";
+
+
+connectDB()
+.then( ()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running at PORT: ${process.env.PORT}`);
+    })
+})
+.catch((err) => console.log("MONGO  CONNECTION faild !!!!" ,err));
