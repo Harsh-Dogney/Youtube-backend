fe58833239cca672ae692a175081b0949e066f41
maxHarsh <harshdogney@gmail.com>
2024-10-02 05:51:29 +0530

    ADD changeCurrentPassword,     getCurrentUser,     updateAccountDetails,     updateUserAvatar,     updateUserCoverImage,     getUserChannelProfile,     getWatchHistory

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
@@ -10,8 +10,8 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
-
 router.route("/refreshToken").post(refreshAccessToken)
+router.route("/changePassword").post(changeCurrentPassword)
 
 
 
